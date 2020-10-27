1b38e84c988bf6a0662021bd51cbcb615206de8a
Shravani Allam Reddy <shravani.allam@gmail.com>
2020-10-26 19:18:24 -0600

    stored the data posted in db.json file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,25 @@
 
 
-module.exports= function(app, notes){
-    app.get("/api/notes", function(req, res) {
-    return res.json(notes);
-  });
+module.exports = function (app, note) {
+    const fs = require('fs');
+    app.get("/api/notes", function (req, res) {
+        return res.json(note);
+    });
 
+    app.post("/api/notes", function (req, res) {
+
+        let newNotes = req.body;
+        
+        // newNotes.title = newNotes.title.replace(/\s+/g, "").toLowerCase();
+        console.log(newNotes);
+
+        note.push(newNotes);
+
+        fs.writeFileSync("./db/db.json", JSON.stringify(note), "utf8");
+
+        res.json(newNotes);
+
+
+    });
 
 }
--- server.js
+++ server.js
@@ -1,20 +1,22 @@
-var express = require("express");
-var path = require("path");
+const express = require("express");
 const fs = require("fs");
-
-var app = express();
-var PORT = 3000;
+const path = require("path");
+const app = express();
+const PORT = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
+
+//Serving static files in Express
+app.use('/', express.static(path.join(__dirname, 'public')))
 app.use(express.json());
 
 
 //Notes
-const notes = JSON.parse(fs.readFileSync("./db/db.json","utf8"));
+const note = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
 //The below points our server to a series of "route" files.
-require("./routes/apiRoutes")(app,notes);
+require("./routes/apiRoutes")(app, note);
 require("./routes/htmlRoutes")(app);
 
 // Starts the server to begin listening
