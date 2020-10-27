ae20b242c5571b7865eb05fe68fdb1909f12656e
Shravani Allam Reddy <shravani.allam@gmail.com>
2020-10-27 13:47:23 -0600

    added code to delete the previously saved notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 
 
 module.exports = function (app, note) {
+    const generateUniqueId = require('generate-unique-id');
     const fs = require('fs');
     app.get("/api/notes", function (req, res) {
         return res.json(note);
@@ -8,8 +9,9 @@ module.exports = function (app, note) {
 
     app.post("/api/notes", function (req, res) {
 
+        req.body.id = generateUniqueId();
         let newNotes = req.body;
-        
+
         // newNotes.title = newNotes.title.replace(/\s+/g, "").toLowerCase();
         console.log(newNotes);
 
@@ -19,7 +21,22 @@ module.exports = function (app, note) {
 
         res.json(newNotes);
 
-
     });
 
+    app.delete("/api/notes/:id", function (req, res) {
+
+        console.log(req.params.id);
+
+        note.map(el => {
+            if (el.id === req.params.id) {
+
+                note.splice(el.id, 1);
+                console.log(note);
+
+            }
+        })
+
+        fs.writeFileSync("./db/db.json", JSON.stringify(note), "utf8");
+    })
 }
+
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -10,7 +10,15 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
+   
     app.get("/notes", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
+
+
+    app.get("/*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+
 }
